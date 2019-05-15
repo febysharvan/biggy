facc1530f134c680533ebbe9aba664edeb2feed1
febysharvan <50619709+febysharvan@users.noreply.github.com>
2019-05-15 08:40:13 +0530

    Create biggy.py

---
+++ biggy.py
@@ -0,0 +1,13 @@
+a,b,c=input( ).split()
+a=int(a);
+b=int(b);
+c=int(c);
+if(a>b):
+    if(a>c):
+        print(int(a));
+    else:
+        print(int(C));
+elif(b>c):
+    print(int(b));
+else:
+    print(int(c));
